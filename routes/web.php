1b30b4463bda022f3e067b00b088952e8c6330a4
rochman25 <zaenur.rochman98@gmail.com>
2020-12-06 13:36:46 +0700

    custom error page

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +18,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::middleware(['guest'])->group(function () {
+    Route::get('/login', [AuthController::class, 'viewLogin'])->name('auth.login');
+    Route::post('/login', [AuthController::class, 'authenticate'])->name('auth.login.action');
+});
+Route::get('/home', [HomeController::class,'index'])->name('view.dashboard');
+// Route::middleware(['auth'])->group(function(){
+//     Route::get('/home', [HomeController::class,'index'])->name('view.dashboard');
+// });
