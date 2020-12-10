6a5e9d384d251bf7931d43291c08c882d5b8a94e
rochman25 <zaenur.rochman98@gmail.com>
2020-12-10 19:59:45 +0700

    init walikelas

---
+++ app/Models/WaliKelas.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class WaliKelas extends Model
+{
+    use HasFactory;
+    protected $table = "wali_kelas";
+    protected $fillable = ["guru_id","kelas_id","tahun_ajaran_id"];
+}
---
+++ database/migrations/2020_12_10_125522_create_wali_kelas_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateWaliKelasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('wali_kelas', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('guru_id');
+            $table->unsignedBigInteger('kelas_id');
+            $table->unsignedBigInteger('tahun_ajaran_id');
+            $table->timestamps();
+
+            $table->foreign('guru_id')->references('id')->on('guru');
+            $table->foreign('kelas_id')->references('id')->on('kelas');
+            $table->foreign('tahun_ajaran_id')->references('id')->on('tahun_ajaran');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('wali_kelas');
+    }
+}
